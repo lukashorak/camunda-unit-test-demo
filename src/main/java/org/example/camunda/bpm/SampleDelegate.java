654a43c17d4997d7958721c553474521f7dc9d3f
Lukas Horak <lukas.horak@gmail.com>
2021-10-27 16:16:38 +0200

    Update code

---
+++ src/main/java/org/example/camunda/bpm/SampleDelegate.java
@@ -0,0 +1,22 @@
+package org.example.camunda.bpm;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+import javax.inject.Named;
+import java.util.logging.Logger;
+
+@Component
+@Named("sampleDelegate")
+@Profile("prod")
+public class SampleDelegate implements JavaDelegate {
+
+    private final Logger LOGGER = Logger.getLogger(SampleDelegate.class.getName());
+    @Override
+    public void execute(DelegateExecution delegateExecution) throws Exception {
+        LOGGER.info("SampleDelegate");
+        // Here is some integration logic
+    }
+}
--- src/test/java/org/example/camunda/bpm/DemoUnitTest.java
+++ src/test/java/org/example/camunda/bpm/DemoUnitTest.java
@@ -1,5 +1,6 @@
 package org.example.camunda.bpm;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.ibatis.logging.LogFactory;
 import org.camunda.bpm.engine.ProcessEngine;
 import org.camunda.bpm.engine.runtime.ProcessInstance;
@@ -14,14 +15,18 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.PostConstruct;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.*;
+import static org.camunda.bpm.engine.test.assertions.bpmn.AbstractAssertions.processEngine;
+import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.assertThat;
 
 
 /**
@@ -63,6 +68,23 @@ public class DemoUnitTest {
     //Task task = task();
     //Task task = processEngine.getTaskService().createTaskQuery().processInstanceId(processInstance.getId()).singleResult();
 
+//    assertThat(processInstance).isActive();
+//
+//    assertThat(task()).hasName("task1");
+//
+//    Map<String, Object> variables1 = new HashMap<>();
+//    variables1.put("var1", false);
+//
+//    complete(task(), variables1);
+
+    firstPartOfTest(processInstance);
+
+
+    assertThat(processInstance).isEnded();
+
+  }
+
+  private void firstPartOfTest(ProcessInstance processInstance){
     assertThat(processInstance).isActive();
 
     assertThat(task()).hasName("task1");
@@ -71,10 +93,31 @@ public class DemoUnitTest {
     variables1.put("var1", false);
 
     complete(task(), variables1);
+  }
 
-    assertThat(processInstance).isEnded();
+  @Test
+  @Deployment(resources = "processTest.bpmn") // only required for process test coverage
+  public void testMessageCorrelation() {
+    // Drive the process by API and assert correct behavior by camunda-bpm-assert
+
+    ProcessInstance processInstance = processEngine().getRuntimeService()
+            .startProcessInstanceByKey("camunda-junit-demo");
+
+
+    assertThat(processInstance).isActive();
+
+    assertThat(task()).hasName("task1");
+
+    processEngine().getRuntimeService().createMessageCorrelation("Message_352c8d1").processInstanceId(processInstance.getId()).correlate();
+
+
+    assertThat(processInstance).isActive();
+
+    assertThat(task()).hasName("task3");
 
   }
+
+
 
   @Test
   @Deployment(resources = "processTest.bpmn") // only required for process test coverage
@@ -106,4 +149,28 @@ public class DemoUnitTest {
     assertThat(processInstance).isEnded();
   }
 
+
+  @Test
+  @Deployment(resources = "processTest.bpmn") // only required for process test coverage
+  public void testHappyPathWithJson() throws IOException {
+    // Drive the process by API and assert correct behavior by camunda-bpm-assert
+
+    ProcessInstance processInstance = processEngine().getRuntimeService()
+            .startProcessInstanceByKey("camunda-junit-demo");
+
+    //Task task = task();
+    //Task task = processEngine.getTaskService().createTaskQuery().processInstanceId(processInstance.getId()).singleResult();
+
+    assertThat(processInstance).isActive();
+
+    assertThat(task()).hasName("task1");
+
+    ObjectMapper objectMapper = new ObjectMapper();
+    Map<String, Object> variables1 = objectMapper.readValue(new ClassPathResource("./task1.json").getFile(), Map.class);
+
+    complete(task(), variables1);
+
+    assertThat(processInstance).isEnded();
+
+  }
 }
